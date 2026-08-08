2f41e99c0a1a8622fa9a390ade5bc808d75c56ee
Julian Lee <julianlee@Julians-MacBook-Pro-2.local>
2026-08-08 01:32:11 -0700

    added layer norm

--- gemv.cpp
+++ gemv.cpp
@@ -1,11 +1,24 @@
 #include <ap_int.h>
 
+template<int ROWS, int COLS>
+void gemv_impl(const unsigned int *W,
+               const unsigned int *x,
+               unsigned long long *out) {
+    for (int r = 0; r < ROWS; r++) {
+        unsigned long long sum = 0;
+        for (int c = 0; c < COLS; c++) {
+#pragma HLS PIPELINE II=1
+            sum += (unsigned long long)W[r * COLS + c] *
+                   (unsigned long long)x[c];
+        }
+        out[r] = sum;
+    }
+}
+
 extern "C" {
 void gemv(const unsigned int *W,
           const unsigned int *x,
-          unsigned long long *out,
-          int rows,
-          int cols) {
+          unsigned long long *out) {
 #pragma HLS INTERFACE m_axi port=W   bundle=gmem0 offset=slave
 #pragma HLS INTERFACE m_axi port=x   bundle=gmem1 offset=slave
 #pragma HLS INTERFACE m_axi port=out bundle=gmem2 offset=slave
@@ -13,19 +26,8 @@ void gemv(const unsigned int *W,
 #pragma HLS INTERFACE s_axilite port=W   bundle=control
 #pragma HLS INTERFACE s_axilite port=x   bundle=control
 #pragma HLS INTERFACE s_axilite port=out bundle=control
-#pragma HLS INTERFACE s_axilite port=rows bundle=control
-#pragma HLS INTERFACE s_axilite port=cols bundle=control
 #pragma HLS INTERFACE s_axilite port=return bundle=control
 
-    for (int r = 0; r < rows; r++) {
-        unsigned long long sum = 0;
-
-        for (int c = 0; c < cols; c++) {
-            sum += (unsigned long long)W[r * cols + c] *
-                   (unsigned long long)x[c];
-        }
-
-        out[r] = sum;
-    }
+    gemv_impl<8, 8>(W, x, out);
 }
 }
--- mac.cpp
+++ mac.cpp
@@ -1,27 +1,32 @@
 #include <ap_int.h>
 
-extern "C" {
-void mac(const unsigned int *in1,
-         const unsigned int *in2,
-         unsigned long long *out,
-         int size) {
-#pragma HLS INTERFACE m_axi port=in1 bundle=gmem0 offset=slave
-#pragma HLS INTERFACE m_axi port=in2 bundle=gmem1 offset=slave
-#pragma HLS INTERFACE m_axi port=out bundle=gmem2 offset=slave
-#pragma HLS INTERFACE s_axilite port=in1 bundle=control
-#pragma HLS INTERFACE s_axilite port=in2 bundle=control
-#pragma HLS INTERFACE s_axilite port=out bundle=control
-#pragma HLS INTERFACE s_axilite port=size bundle=control
-#pragma HLS INTERFACE s_axilite port=return bundle=control
-#pragma HLS ARRAY_PARTITION variable=in1
-#pragma HLS ARRAY_PARTITION variable=in2
+template<int SIZE>
+void mac_impl(const unsigned int *in1,
+              const unsigned int *in2,
+              unsigned long long *out) {
+    unsigned long long sum = 0;
 
-unsigned long long sum = 0;
-    for (int i = 0; i < size; i++) {
-        #pragma HLS PIEPLINE II=1
+    for (int i = 0; i < SIZE; i++) {
+#pragma HLS PIPELINE II=1
         sum += (unsigned long long)in1[i] * (unsigned long long)in2[i];
     }
 
     *out = sum;
+}
+
+extern "C" {
+void mac(const unsigned int *in1,
+         const unsigned int *in2,
+         unsigned long long *out) {
+#pragma HLS INTERFACE m_axi port=in1 bundle=gmem0 offset=slave
+#pragma HLS INTERFACE m_axi port=in2 bundle=gmem1 offset=slave
+#pragma HLS INTERFACE m_axi port=out bundle=gmem2 offset=slave
+
+#pragma HLS INTERFACE s_axilite port=in1 bundle=control
+#pragma HLS INTERFACE s_axilite port=in2 bundle=control
+#pragma HLS INTERFACE s_axilite port=out bundle=control
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+
+    mac_impl<64>(in1, in2, out);
 }
 }
